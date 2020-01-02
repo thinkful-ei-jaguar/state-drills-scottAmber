4d1985a0f797f118eab42b72b5811e060362dc00
amberdoo11 <57081110+amberdoo11@users.noreply.github.com>
2020-01-02 11:59:05 -0700

    first drill

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import HelloWorld from './state-drills/HelloWorld';
+
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+
